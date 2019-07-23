71de8ae5f034e8412a77dfcbb9af6907057aab3e
Eoin O'Kane <eoinandrewokane@gmail.com>
2019-07-23 07:43:12 -0700

    Improve the readability of the ip route show command
    Signed-off-by: Eoin O'Kane <eoinandrewokane@gmail.com>

--- cmds/core/ip/ip.go
+++ cmds/core/ip/ip.go
@@ -214,7 +214,101 @@ func routeshow() error {
 	if err != nil {
 		return fmt.Errorf("Route show failed: %v", err)
 	}
-	log.Printf("%s", string(b))
+	// Need to formation this better.
+	if !(*inet6) {
+	/*
+		ip:
+		0			1						2					3			4				5		6				7					8		9				10
+		Iface	Destination	Gateway 	Flags	RefCnt	Use	Metric	Mask			MTU	Window	IRTT
+  	ens33	00000000		02C210AC	0003	0				0		100			00000000	0		0				0
+  	ens33	0000FEA9		00000000	0001	0				0		1000		0000FFFF	0		0				0
+  	ens33	00C210AC		00000000	0001	0				0		100			00FFFFFF	0		0				0
+
+		want to turn it into something like this
+
+		eoinokane@ubuntu:~/.gvm/pkgsets/go1.12/global/src/github.com/u-root/u-root/cmds/core/ip$ ip route
+		default via 172.16.194.2 dev ens33 proto dhcp metric 100
+		169.254.0.0/16 dev ens33 scope link metric 1000
+		172.16.194.0/24 dev ens33 proto kernel scope link src 172.16.194.129 metric 100
+
+		TODO:
+		* Figure out where the scope SCOPE_VAL comes from
+		 		<snip>
+				scope SCOPE_VALUE --- scope of the area within which this address is valid.
+				The available scopes are listed in the file
+				/etc/iproute2/rt_scopes. The predefined scope values are:
+				global --- the address is globally valid.
+				site --- (IPv6 only) address is site local, valid only inside this site.
+				link --- the address is link local, valid only on this device.
+				host --- the address is valid only inside this host.
+				</snip>
+		* Figure out where the protocol RTPPROTO comes from
+		  	according to http://www.policyrouting.org/iproute2.doc.html#ss9.5.5
+				<snip>
+				protocol RTPROTO --- routing protocol identifier of this route. RTPROTO
+				may be a number or a string from the file /etc/iproute2/rt_protos. If the
+				routing protocol ID is not given ip assumes the protocol is boot. IE.
+				This route has been added by someone who does not understand what they are doing.
+				Several of these protocol values have a fixed interpretation.
+				<snip>
+		* Confirm of dev NAME (default) --- NAME specifies the network device to operate on
+
+		*/
+		// Split the string by new line (Assumed format above)
+		rows := strings.Split(string(b),"\n")
+
+		// Get the headers
+		row := strings.Fields(rows[0])
+
+		// Set a boolean for the end of file
+		eof := false
+
+		// Skip the top line, (already verified with regex)
+		for v :=1; v < len(rows) && !eof; v++ {
+			if rows[v]=="" {
+
+				// Found the end of the file
+				eof = true
+			} else {
+
+				// Get the cols, can make some assumptions based on the regex match used
+				row = strings.Fields(strings.Trim(rows[v]," "))
+
+				// Create a simple array to hold the output
+				var o []string
+
+				// Get the source ip address, it's in hex format.
+				if src_address, err := hextoipaddress(row[1]); err!=nil {
+					flog.Printf("Cannot decode the source ip address %v",err)
+					return err
+				} else {
+					o = append(o, src_address)
+					if gateway_address, err := hextoipaddress(row[2]); err!=nil {
+						flog.Printf("Cannot decode the gateway ip address %v",err)
+						return err
+					}	else {
+						o = append(o, gateway_address)
+						if mask_address,err := hextoipaddress(row[7]); err!=nil {
+							flog.Printf("Cannot decode the mask ip address %v",err)
+							return err
+						} else {
+							mask_address := net.IPMask(net.ParseIP(mask_address).To4())
+							mask_size, _ := mask_address.Size()
+							if mask_size != 0 {
+								o[0] += "/"
+								o[0] += strconv.Itoa(mask_size)
+							}
+							o = append(o, row[0])
+							o = append(o, row[6])
+						}
+					}
+				}
+				flog.Printf("%s via %s dev %s ?proto? ?dhcp? metric %s",o[0],o[1],o[2],o[3])
+			}
+		}
+	} else {
+		flog.Printf("%s", string(b))
+	}
 	return nil
 }
 
